acbf8783a8921997e63384400c1859c5d1be4b78
lelik9 <lelik9@yandex.ru>
2017-04-26 23:39:00 +0300

    add base cookbook

--- cookbooks/first-task/metadata.rb
+++ cookbooks/first-task/metadata.rb
@@ -5,3 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures first-task'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+depends          'iptables'
+depends          'apt'
+depends          'ntp'
+depends          'sudo'
--- cookbooks/first-task/recipes/default.rb
+++ cookbooks/first-task/recipes/default.rb
@@ -6,9 +6,52 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe 'iptables'
+include_recipe 'ntp'
+include_recipe 'apt'
+include_recipe 'sudo'
+
 apt_update
 
 apt_package 'base' do
   action :install
   package_name ['htop', 'wget', 'git', 'iotop', 'tcpdump', 'vim', 'sysstat']
+end
+
+user 'user' do
+  username node['user']
+  home  '/home/'+node['user']
+  shell '/bin/bash'
+  manage_home true
+  action :create
+end
+
+directory '/home/'+node['user']+'/.ssh' do
+  action :create
+end
+
+template '/home/'+node['user']+'/.ssh/authorized_keys' do
+  atomic_update true
+  source 'id_rsa.pub' 
+end
+
+bash 'ssh' do
+  code <<-EOL
+	sed -e 's/UsePAM/UsePAM no/' /etc/ssh/sshd_config >> /etc/ssh/sshd_config.new
+	cp /etc/ssh/sshd_config /etc/ssh/sshd_config.old; mv /etc/ssh/sshd_config.new /etc/ssh/sshd_config
+	EOL
+end
+
+service 'sshd' do
+  action :reload
+end
+
+bash 'clearMOTD' do
+ code 'rm -rf etc/update-motd.d/*'
+end
+
+cookbook_file '/etc/update-motd.d/00-header' do
+  source '00-header'
+  mode '0777'
+  action :create
 end
